2d9cf50028d8607536a0ba9adf43221f8994c0a2
Facundo735 <Facundo.sanzone@gmail.com>
2025-09-09 16:45:34 -0300

    Update Chatbot.py

--- Chatbot/Chatbot.py
+++ Chatbot/Chatbot.py
@@ -6,20 +6,13 @@ import groq #api
 
 #tener nuestros modelos de IA
 
-modelos = ['llama3-8b-8192', 'llama3-70b-8192']
+modelos = ['llama3-8b-8192', 'llama3-70b-8192','mixtral-8x7b-32768']
 
 
 #funcion para configurar la pagina
 def configurar_pagina ():
     st.set_page_config(page_title="Pagina con python",page_icon="🫡")# page_title para cambiar el nombre de la pagina y page icon para el icono
-    st.title ("Pagina para el chatbot")     
-
-#un cliente groq
-
-def crear_cliente_groq():
-    groq_api_key = st.secrets["GROQ_API_KEY"] #almacenar la api key de groq
-    return groq.Groq(api_key = groq_api_key)
-    
+    st.title ("Pagina para el chatbot")                  
 
 #mostrar el sidebar con los modelos
 def mostrar_sidebar ():
@@ -27,13 +20,19 @@ def mostrar_sidebar ():
     modelo = st.sidebar.selectbox("Cual elegis?",modelos, index=0)
     st.write(f"Elegiste el modelo:{modelo}")
     return modelo
+    
 
+#un cliente groq
+
+def crear_cliente_groq():
+    groq_api_key = st.secrets["GROQ_API_KEY"] #almacenar la api key de groq
+    return groq.Groq(api_key = groq_api_key)
 
 #INICIALIZAR EL ESTADO DE LOS MENSAJES
 
 def inicializacion_estado_chat ():
 
-    if "mensajes" not in st.session_state:
+    if "mensajes" not in st.session_state.mensajes:
         st.session_state.mensajes = [] #lista
 #muestra mensajes previos
 
@@ -41,7 +40,7 @@ def inicializacion_estado_chat ():
 #Historial del chat 
 
 def mostrar_historial ():
-    for mensaje in st.session_state.mensajes:
+    for mensaje in st.session_state.mensaje:
         with st.chat_message(mensaje["role"]): #quien lo envia
             st.markdown(mensaje["content"]) #que envia
 
@@ -71,26 +70,20 @@ def obtener_respuesta_modelo(cliente, modelo, mensajes):
     return respuesta.choices[0].message.content
 
 #flujo de la app
+
 def ejecutar_app():
     configurar_pagina()
-    cliente = crear_cliente_groq()
     modelo = mostrar_sidebar()
-
+    cliente = crear_cliente_groq()
+    #inicializacion_estado_chat
     inicializacion_estado_chat()
-    mostrar_historial()
-
     mensaje_usuario = obtener_mensaje()
-    if mensaje_usuario:
-        agregar_mensaje_historial("user", mensaje_usuario)
-        mostrar_mensaje("user", mensaje_usuario)
-
-        respuesta = obtener_respuesta_modelo(cliente, modelo, st.session_state.mensajes)
-        agregar_mensaje_historial("assistant", respuesta)
-        mostrar_mensaje("assistant", respuesta)
+    obtener_mensaje()
 
 #ejecutar la api
 if __name__ == "__main__": #si este archivo es el principal entonces ejecuta
     ejecutar_app()
+
 
 
 
